0dc8b2596973feb69bccdb3532df0704fe864a36
Luis Silva <luisadas1@hotmail.com>
2023-08-17 21:41:29 +0100

    done

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         React JS Phase 5 project deployment Jenkins-Docker-EC2 instance luis.silvasoto@vodafone.com
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
